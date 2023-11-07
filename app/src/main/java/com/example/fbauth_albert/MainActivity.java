d283d22d47397bb71796aa73baebf4a9b68b7160
Dan Zhuravlov <yuoirty@gmail.com>
2023-11-07 15:15:27 +0200

    Finished design

---
+++ app/src/main/java/com/example/fbauth_albert/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fbauth_albert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
